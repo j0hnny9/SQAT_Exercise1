c8852b33c3e3be97997015f91104439004ac329f
somename <someemail>
2016-09-23 15:00:11 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -15,13 +15,13 @@ public class TestBowling {
 */	
 	
 	@Test
-	public void testFrame_InitialSetup_Frame_2_4_output_firstThrow2() {
+	public void testFrame_InitialSetup_Frame_2_4_output_firstThrow2() throws BowlingException {
 		Frame testFrame = new Frame(2,4);
 		assertEquals(2, testFrame.getFirstThrow());
 	}
 	
 	@Test
-	public void testFrame_InitialSetup_Frame_2_4_output_SecondThrow4() {
+	public void testFrame_InitialSetup_Frame_2_4_output_SecondThrow4() throws BowlingException {
 		Frame testFrame = new Frame(2,4);
 		assertEquals(4, testFrame.getSecondThrow());
 	}
@@ -30,5 +30,7 @@ public class TestBowling {
 	public void testFrame_InitialSetup_InvalidThrowNr_Exception() throws BowlingException {
 		Frame testFrame = new Frame(2,4);
 	}
+	
+	
 
 }
