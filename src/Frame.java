442e9facb0f291adfa947fd42925b3c63100c137
somename <someemail>
2016-09-23 16:22:09 +0300

    besouro automatic message

--- src/Frame.java
+++ src/Frame.java
@@ -1,6 +1,7 @@
 public class Frame {
 	private int firstThrow;
 	private int secondThrow;
+	Frame subsequentFrame;
 	
 	public Frame(int firstThrow, int secondThrow) throws BowlingException {
 		if (firstThrow < 0 || secondThrow < 0) {
@@ -22,7 +23,20 @@ public class Frame {
 
 	//returns the score of a single frame
 	public int score() {
-		return firstThrow + secondThrow;
+		int score = firstThrow + secondThrow;
+		
+		/*
+		 *  check if there is a subsequent frame
+		 *   for the case of a strike or spare frame and calculate score for these cases
+		 *  otherwise do nothing
+		 */
+		if (subsequentFrame != null && this.isStrike()) {					
+			score += subsequentFrame.getFirstThrow() + subsequentFrame.getSecondThrow();
+		} else if (subsequentFrame != null && this.isSpare()) {
+			score += subsequentFrame.getFirstThrow();
+		}
+		
+		return score; 
 	}
 
 	//returns whether the frame is a strike or not
@@ -41,5 +55,9 @@ public class Frame {
 		} else {		
 			return false;
 		}
+	}
+	
+	public void setSubsequentFrame(Frame subsequentFrame) {
+		this.subsequentFrame = subsequentFrame;
 	}
 }
