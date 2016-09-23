aa43b4816ec130d46253c9ab444a13b2295f78c1
somename <someemail>
2016-09-23 15:20:14 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -14,6 +14,10 @@ public class TestBowling {
 	}
 */	
 	
+	/*
+	 * 1. Frame
+	 */
+	
 	@Test
 	public void testFrame_InitialSetup_Frame_2_4_output_firstThrow2() throws BowlingException {
 		Frame testFrame = new Frame(2,4);
@@ -38,9 +42,13 @@ public class TestBowling {
 	
 	@Test(expected=BowlingException.class)
 	public void testFrame_InvalidSumOfThrows_Exception() throws BowlingException {
-		Frame testFrame = new Frame(11,4);
+		Frame testFrame = new Frame(5,6);
 	}
 	
+	
+	/*
+	 * 2. Frame Score
+	 */
 	
 	@Test
 	public void testFrame_score_InitialSetup_Frame_2_4_output_6() throws BowlingException {
@@ -49,9 +57,32 @@ public class TestBowling {
 	}
 	
 	@Test
-	public void testFrame_score_InitialSetup_Frame_2_4_output_6() throws BowlingException {
-		Frame testFrame = new Frame(2,4);
-		assertEquals(6, testFrame.score());
+	public void testFrame_score_InitialSetup_Frame_7_1_output_8() throws BowlingException {
+		Frame testFrame = new Frame(7,1);
+		assertEquals(8, testFrame.score());
+	}
+	
+	
+	/*
+	 * 3. Game
+	 */
+	
+	@Test
+	public void testGame_add3Frames_returnSecondFrameFirstThrow() throws BowlingException {
+		BowlingGame game = new BowlingGame();
+		game.addFrame(new Frame(0,4));
+		game.addFrame(new Frame(1,3));
+		game.addFrame(new Frame(2,8));
+		assertEquals(1, game.getExistingFrameByNr(1).getFirstThrow());
+	}
+	
+	@Test
+	public void testGame_add10Frames_returnFourthFrameSecondThrow() throws BowlingException {
+		BowlingGame game = new BowlingGame();
+		for (int i=0; i<10; i++) {
+			game.addFrame(new Frame(0,i));
+		}
+		assertEquals(3, game.getExistingFrameByNr(3).getSecondThrow());
 	}
 
 }
